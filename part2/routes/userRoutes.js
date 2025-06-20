18dede8831894c3c6713b59f50bc7f5e01556898
VANSHIKA TAYAL <a1945792@adelaide.edu.au>
2025-06-20 04:14:16 +0000

    [automatic commit] 2025-06-20 04:14:16 UTC 175.45.84.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -40,7 +40,10 @@ router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    
+    const [rows] = await db.query(
+      `SELECT user_id, username, role FROM Users WHERE email = ? AND password_hash = ?`,
+      [email, password]
+    );
 
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
