487eba270eccb97285df331abe6f04945d36113d
VANSHIKA TAYAL <a1945792@adelaide.edu.au>
2025-06-20 03:26:06 +0000

    [automatic commit] 2025-06-20 03:26:06 UTC 175.45.84.98

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -1,4 +1,17 @@
-{
-    path: '/login',
-    component: LoginPage
-  }
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginPage from '../components/LoginPage.vue';
+import OwnerDashboard from '../components/OwnerDashboard.vue';
+import WalkerDashboard from '../components/WalkerDashboard.vue';
+
+const routes = [
+  { path: '/', component: LoginPage },
+  { path: '/owner-dashboard', component: OwnerDashboard },
+  { path: '/walker-dashboard', component: WalkerDashboard }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
