e722b6dc3b23d5ba97cbcea096dc47c349b242bf
VANSHIKA TAYAL <a1945792@adelaide.edu.au>
2025-06-20 03:16:20 +0000

    [automatic commit] 2025-06-20 03:16:20 UTC ;; communications error to 208.67.222.222#53: timed out 175.45.84.98

--- part2/app.js
+++ part2/app.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+
+// ✅ Add session support
+app.use(session({
+    secret: 'dogwalksecret', // Use env var for production
+    resave: false,
+    saveUninitialized: true
+  }));
+
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
