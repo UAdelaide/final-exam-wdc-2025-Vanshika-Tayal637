13b471c463f1ad792cbfc1d941275748933bb791
VANSHIKA TAYAL <a1945792@adelaide.edu.au>
2025-06-21 06:21:04 +0000

    [automatic commit] 2025-06-21 06:21:04 UTC 1.147.53.196

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -35,24 +35,6 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-// // GET dogs owned by the currently logged in owner
-// router.get('/my-dogs', async (req, res) => {
-//   if (!req.session.user) {
-//     return res.status(401).json({ error: 'Not logged in' });
-//   }
-
-//   const ownerId = req.session.user.user_id;
-
-//   try {
-//     const [rows] = await db.query(
-//       'SELECT dog_id, name FROM Dogs WHERE owner_id = ?',
-//       [ownerId]
-//     );
-//     res.json(rows);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Failed to fetch dogs' });
-//   }
-// });
 
 // POST login
 router.post('/login', async (req, res) => {
